51805dca55f5393c363a107399bafeb162b92acf
WebDiyar <diyar.atashka@mail.ru>
2023-11-13 18:38:13 +0600

    diyar

--- index.js
+++ index.js
@@ -118,8 +118,6 @@ updateCountdown();
 slider();
 
 
-
-
 // const heart = document.getElementById('heart');
 // const heart1 = document.getElementById('heart1');
 // const heart2 = document.getElementById('heart2');
